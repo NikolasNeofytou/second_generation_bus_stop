0ec9a69fd7655318de63adc33cbc7c94a3c554ba
Nikolas Neofytou <87272977+NikolasNeofytou@users.noreply.github.com>
2025-08-23 21:50:26 +0300

    feat: add i18n support with language switcher

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,29 @@
 import Head from 'next/head';
 import React from 'react';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import LanguageSwitcher from '../components/LanguageSwitcher';
 
 export default function Home() {
+  const { t } = useTranslation('common');
   return (
     <>
       <Head>
-        <title>Cyprus Bus Stop</title>
+        <title>{t('title')}</title>
       </Head>
       <main>
-        <h1>Cyprus Bus Stop App</h1>
-        <p>Coming soon...</p>
+        <h1>{t('heading')}</h1>
+        <p>{t('comingSoon')}</p>
+        <LanguageSwitcher />
       </main>
     </>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
 }
